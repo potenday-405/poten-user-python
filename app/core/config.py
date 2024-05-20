9ac789548fb9a3afddbd814c396196cd8990f777
chilln <kimjihan77@naver.com>
2024-05-20 15:22:24 +0900

    Feat:config.py - create environment variable

--- app/core/config.py
+++ app/core/config.py
@@ -1,1 +1,9 @@
-# 인증인가
+import os
+from dotenv import load_dotenv
+
+# .env 파일을 로드
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
