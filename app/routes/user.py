4501402780e8a5ff7c91fb03ab257e675f254fa9
chilln <kimjihan77@naver.com>
2024-05-21 04:45:16 +0900

    Feat:login API

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, Request,Header, HTTPException, Depends
+from fastapi import APIRouter, Request,Header, HTTPException, Depends, status
 from sqlalchemy.orm import Session
 from app.database.settings import SessionLocal, engine
 from app.services.user import UserService
 from app.core.db import Engineconn
-from app.models.user import UserSignup, UserSignupResponse
+from app.models.user import UserSignup, UserSignupResponse, UserToken
 
 engine = Engineconn()
 session = engine.create_session()
@@ -33,3 +33,28 @@ async def signup(user:UserSignup, db:Session = Depends(get_test_db), version: st
     await user_service.create_user(user)
 
     return UserSignupResponse(message="회원가입이 완료되었습니다.")
+
+@router.post("/login")
+async def create_token(email:str, password:str, db:Session = Depends(get_test_db), version: str = Header("1.0")) -> UserToken:
+    """로그인 시 accessToken 발행"""
+
+    user_service = UserService(db)
+
+    # 유효한 회원인지 체크
+    is_validate = await user_service.check_validate_user(email=email, password=password)
+    if not is_validate:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    # accesstoken 발행
+    access_token = user_service.create_access_token(data={"sub": email})
+
+    return UserToken(
+        access_token=access_token,
+        token_type="bearer"
+    )
+    
+    
--- app/services/user.py
+++ app/services/user.py
@@ -2,7 +2,11 @@ from sqlalchemy.orm import Session
 from app.database import models
 from app.models.user import UserSignup
 from sqlalchemy import insert
-# from sqlalchemy import and_, delete, func, insert, select, text, update
+from app.utils.user import get_hashed_password
+from jose import JWTError, jwt
+from typing import Optional
+from datetime import datetime, timedelta
+from app.core.config import ACCESS_TOKEN_EXPIRE_MINUTES, SECRET_KEY, ALGORITHM
 
 class UserService():
     def __init__(self, db:Session):
@@ -14,7 +18,7 @@ class UserService():
         :return : 유저 정보
         """
         User = models.User
-        user = self.db.query(User).filter(User.email == email).all()
+        user = self.db.query(User).filter(User.email == email).first()
         return user
 
     async def create_user(self, user:UserSignup):
@@ -22,6 +26,7 @@ class UserService():
         self.db.execute(
             insert(models.User).values(
             **user.dict(),
+            # password=get_hashed_password(user.password),
             user_status='act'
         ))
 
@@ -29,5 +34,29 @@ class UserService():
         
         return user
 
+    async def check_validate_user(self, email:str, password):
+        """이메일로 사용자 조회"""
+        User = models.User
+        user = self.db.query(User).filter(
+            User.email == email,
+            User.user_password == password
+        ).first()
+        return user
+
     async def get_users(self):
         return self.db.query(models.User).all()
+
+    #TODO auth쪽은 따로 분리할 것
+    # JWT 토큰 생성 함수 
+    def create_access_token(self, data: dict):
+
+        expires_delta = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+        to_encode = data.copy()
+        if expires_delta:
+            expire = datetime.utcnow() + expires_delta
+        else:
+            expire = datetime.utcnow() + timedelta(minutes=15)
+        to_encode.update({"exp": expire})
+        encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+        return encoded_jwt
