bcf57987c4d6b6f76a4cf9858624ee8bd0a4c8cc
chilln <kimjihan77@naver.com>
2024-05-21 02:51:58 +0900

    Feat:DB info environment Variables

--- app/core/config.py
+++ app/core/config.py
@@ -1,4 +1,5 @@
 import os
+from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
 
 # .env 파일을 로드
@@ -7,3 +8,9 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+
+TEST_DB_USER = os.getenv("TEST_DB_USER")
+TEST_DB_PASSWORD = os.getenv("TEST_DB_PASSWORD")
+TEST_DB_HOST = os.getenv("TEST_DB_HOST")
+TEST_DB_PORT = 3306
+TEST_DB_BASE = os.getenv("TEST_DB_BASE")
