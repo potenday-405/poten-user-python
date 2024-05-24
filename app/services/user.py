b21f5da2e8b02eb874dcc1501023278f696f8963
chilln <kimjihan77@naver.com>
2024-05-24 19:20:34 +0900

    Fix:modify token expire time

--- app/services/user.py
+++ app/services/user.py
@@ -58,7 +58,7 @@ class UserService():
         if expires_delta:
             expire = datetime.utcnow() + expires_delta
         else:
-            expire = datetime.utcnow() + timedelta(minutes=30)
+            expire = datetime.utcnow() + timedelta(days=30)
         to_encode.update({"exp": expire})
         encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
         return encoded_jwt
